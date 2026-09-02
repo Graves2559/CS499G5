a608a3a3d37cebdcf489e334539cc3620ae9d9a2
GodlyEndurance <intermediateintern@gmail.com>
2026-09-02 00:16:21 -0500

    Introduction of WI7

---
+++ TrinhPham/WI7/main.py
@@ -0,0 +1,24 @@
+
+
+'''
+main.py
+
+This is the executable script for the project.
+This will instantiate the main classes of the project and inject the necessary dependencies.
+
+
+'''
+
+'''
+Context: 
+1. MongoDB is a NoSQL database that stores data in JSON, BSON, and XML documents.
+2. NoSQL databases allow for horizontal scaling, such that there can be many servers instead of just one for storing data.
+'''
+
+'''
+Objectives:
+1. To import data into a MongoDB
+    -> Requires a running MongoDB instance and code to connect to it.
+2. Have mock data to import into the MongoDB database.
+
+'''
